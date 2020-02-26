e5a41bc05fa9da4d3ea3efbb8003fc48e74e2b3c
Cleber Reis <cleberreisdeveloper@gmail.com>
2020-02-26 16:53:25 -0300

    primeiro commit, inicio projeto

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('Transferências'),
+    ),
+    body: ListaTransferencias(),
+    floatingActionButton: FloatingActionButton(
+      child: Icon(Icons.add),
+    ),
+  ),
+));
+
+class ListaTransferencias extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(
+          Transferencia(100.0, 1000),
+        ),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget{
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text(_transferencia.valor.toString()),
+          subtitle: Text(_transferencia.numeroConta.toString()),
+        )
+    );
+  }
+}
+
+class Transferencia{
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
